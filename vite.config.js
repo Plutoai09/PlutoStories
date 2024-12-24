e587e567ec4048ade761fa271e36b1c30370d032
ZaidBJ <42514294+ZaidBJ@users.noreply.github.com>
2024-12-24 19:51:14 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        'service-worker': 'service-worker.js'
+        'service-worker': '/public/service-worker.js'
       }
     }
   },
